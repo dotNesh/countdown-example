1e2953400cbabfe121d5cbd400cfe2fbe699abc1
Kelvin Kariuki <kmunene@live.com>
2019-04-04 16:01:01 +0300

    setting up the directory

---
+++ App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import EventList from './EvenList';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <EventList />
+    )
+  }
+}
