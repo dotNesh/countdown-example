d87cf8d13266c363ef2e92b490442acb2ff2f574
Kelvin Kariuki <kmunene@live.com>
2019-04-10 15:50:15 +0300

    Creating the component and styling

---
+++ EventCard.js
@@ -0,0 +1,102 @@
+import React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+// import PropTypes from ' prop-types';
+import { formatDate, getCountdownParts } from './api';
+
+
+const styles = StyleSheet.create({
+    card: {
+        backgroundColor: '#fff',
+        flex: 1,
+        padding: 10,
+        paddingTop: 10,
+        paddingBottom: 20,
+        margin: 10,
+        marginTop: 5,
+        marginBottom: 5,
+    },
+    cardHeader: {
+        flex: 1,
+        flexDirection: 'row'
+    },
+    date: {
+        fontWeight: '200',
+        fontSize: 15,
+        color: '#bdbdbd',
+        width: '30%',
+        textAlign: 'right'
+    },
+    title: {
+        fontSize: 15,
+        fontWeight: '300',
+        marginLeft: 7,
+        textAlign: 'left'
+    },
+    counterContainer:{
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        paddingLeft: '5%',
+        paddingRight: '5%'
+    },
+    counter: {
+        width: '25%',
+        flex: 1,
+    },
+    counterText: {
+        fontSize: 40,
+        textAlign: 'center'
+    },
+    counterLabel: {
+        fontSize: 13,
+        fontWeight: '100',
+        color: '#a3a3a3',
+        textAlign: 'center',
+        paddingTop: 0
+    }
+    
+})
+
+export default function EventCard({ event }){
+    const {
+        days,
+        hours,
+        minutes,
+        seconds,
+    } = getCountdownParts(event.date);
+
+    return(
+        <View style={styles.card}>
+            <View style={styles.cardHeader}>
+                <Text style={styles.date}>{formatDate(event.date)}</Text>
+                <Text style={styles.title}>{event.title}</Text>
+            </View>
+            <View style={styles.counterContainer}>
+                <View styles={styles.counter}>
+                    <Text styles={styles.counterText}>{days}</Text>
+                    <Text styles={styles.counterLabel}>Days</Text>
+                </View>
+                <View styles={styles.counter}>
+                    <Text styles={styles.counterText}>{hours}</Text>
+                    <Text styles={styles.counterLabel}>Hours</Text>
+                </View>
+                <View styles={styles.counter}>
+                    <Text styles={styles.counterText}>{minutes}</Text>
+                    <Text styles={styles.counterLabel}>Minutes</Text>
+                </View>
+                <View styles={styles.counter}>
+                    <Text styles={styles.counterText}>{seconds}</Text>
+                    <Text styles={styles.counterLabel}>Seconds</Text>
+                </View>
+            </View>
+        </View>
+    )
+}
+
+
+// EventCard.propTypes = {
+//     event: PropTypes.shape({
+//         title: PropTypes.string.isRequired,
+//         date: PropTypes.instanceOf(Date)
+//     }),
+// }
