890d98e41ec98d46666fbcde3be6045d7eea7496
Kelvin Kariuki <kmunene@live.com>
2019-04-10 17:22:39 +0300

    getting all events

--- api.js
+++ api.js
@@ -1,4 +1,20 @@
 import moment from 'moment';
+import uuid from 'uuid';
+
+import Expo from "expo";
+
+const { manifest } = Expo.Constants;
+const api = manifest.packagerOpts.dev
+  ? manifest.debuggerHost.split(`:`).shift().concat(`:3000`)
+  : 'api.example.com';
+
+const url = `http://${api}/events`;
+
+export function getEvents() {
+  return fetch(url)
+  .then(response => response.json())
+  .then(events => events.map(e => ({ ...e, date: new Date(e.date) })))
+}
 
 export function formatDate(dateString){
     const parsed = moment(new Date(dateString));
