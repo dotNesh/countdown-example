f2acee73e675eca6262b8bca4cb83f0a96c2f510
Kelvin Kariuki <kmunene@live.com>
2019-04-10 17:15:42 +0300

    Implement stack navigation between event list and event form

--- App.js
+++ App.js
@@ -1,10 +1,26 @@
-import React from 'react';
-import EventList from './EvenList';
+import {
+  StackNavigator,
+} from 'react-navigation';
+import EventList from './EventList';
+import EventForm from './EventForm';
+import { YellowBox } from 'react-native';
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <EventList />
-    )
-  }
-}
+YellowBox.ignoreWarnings([
+  'Warning: componentWillMount is deprecated',
+  'Warning: componentWillReceiveProps is deprecated',
+]);
+
+export default StackNavigator({
+  list: {
+    screen: EventList,
+    navigationOptions: () => ({
+      title: 'Your events',
+    }),
+  },
+  form: {
+    screen: EventForm,
+    navigationOptions: () => ({
+      title: 'Add an event',
+    }),
+  },
+});
